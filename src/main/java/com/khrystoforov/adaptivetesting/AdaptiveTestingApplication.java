d5c4167ffb0c3725a4cf640f0c215a24aef8b82a
HrisKirill <hristoforow.kirill@gmail.com>
2025-02-20 10:17:20 +0200

    init

---
+++ src/main/java/com/khrystoforov/adaptivetesting/AdaptiveTestingApplication.java
@@ -0,0 +1,13 @@
+package com.khrystoforov.adaptivetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptiveTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptiveTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khrystoforov/adaptivetesting/AdaptiveTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khrystoforov.adaptivetesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptiveTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
