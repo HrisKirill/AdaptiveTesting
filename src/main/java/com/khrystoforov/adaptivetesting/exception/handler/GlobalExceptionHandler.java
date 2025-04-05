c675c637bd9e0104131461d9dd302dc5ecdcc436
HrisKirill <hristoforow.kirill@gmail.com>
2025-04-05 13:06:27 +0300

    add handler for throwable

--- src/main/java/com/khrystoforov/adaptivetesting/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/khrystoforov/adaptivetesting/exception/handler/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.khrystoforov.adaptivetesting.exception.handler;
 import com.khrystoforov.adaptivetesting.exception.EntityExistsException;
 import com.khrystoforov.adaptivetesting.exception.EntityNotFoundException;
 import com.khrystoforov.adaptivetesting.exception.ErrorResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+    private final Environment environment;
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -25,5 +29,12 @@ public class GlobalExceptionHandler {
     public ErrorResponse handleException(EntityExistsException ex) {
         log.error(ex.getMessage());
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ErrorResponse handleException(Throwable ex) {
+        log.error(ex.getMessage());
+        boolean isProduction = environment.matchesProfiles("production");
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), isProduction ? "" : ex.getMessage());
     }
 }
